7bc9fd3f26ed0782e3d5ca9a44009d3d6b179a57
Viktor Tsirkin <reset1985@mail.ru>
2022-09-17 22:04:27 +0300

    add solution Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введи число: ");
+int n = int.Parse(Console.ReadLine()); //записали введенное число в переменную n
+int a = 1;
+while (a <= n)
+{
+    if (a % 2 == 0)
+    {
+        Console.Write(a+";");
+        a++;
+    }
+    else
+    {
+        Console.Write(" ");
+        a++;
+    }
+}
