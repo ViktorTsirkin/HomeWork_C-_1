8233dc0882e75a3c915b5b5417e02bc5b954ef2c
Viktor Tsirkin <reset1985@mail.ru>
2022-09-17 21:59:34 +0300

    add solution Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введи первое число: ");
+int x = int.Parse(Console.ReadLine()); //записали введенное число в переменную x
+Console.WriteLine("Введи второе число: ");
+int y = int.Parse(Console.ReadLine()); //записали введенное число в переменную y
+Console.WriteLine("Введи третье число: ");
+int z = int.Parse(Console.ReadLine()); //записали введенное число в переменную z
+int m = x;
+if (x > y)
+{
+    m = x;
+}
+else
+{
+    m = y;
+}
+if (m > z)
+{
+    Console.WriteLine($"максимальное число {m}");
+}
+else
+{
+    Console.WriteLine($"максимальное число {z}");
+}
