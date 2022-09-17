20381f28b8ca93fceadf1fc6a296f4d427712969
Viktor Tsirkin <reset1985@mail.ru>
2022-09-17 21:55:22 +0300

    add solution Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введи первое число: ");
+int x = int.Parse(Console.ReadLine()); //записали введенное число в переменную x
+Console.WriteLine("Введи второе число: ");
+int y = int.Parse(Console.ReadLine()); //записали введенное число в переменную y
+if (x > y)
+{
+    Console.WriteLine($"Число {x} больше числа {y}");
+}
+else
+{
+    Console.WriteLine($"Число {y} больше числа {x}");
+}
