f6a45505dcd3a6a56442c6800ba35e25ea8b0638
Viktor Tsirkin <reset1985@mail.ru>
2022-09-17 22:01:54 +0300

    add solution Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введи число: ");
+int x = int.Parse(Console.ReadLine()); //записали введенное число в переменную x
+if (x % 2 == 0)
+{
+    Console.WriteLine($"Число {x} - четное");
+}
+else
+{
+    Console.WriteLine($"Число {x} - нечетное");
+}
